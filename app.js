c58968739bc7380d5db9655ccb0c9567dc157d8a
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-01-24 15:41:49 -0500

    completed app

--- app.js
+++ app.js
@@ -10,21 +10,30 @@ async function fetchZone(zip) {
 }
 
 submitButton.addEventListener('click', async () => {
-    
+
     let zipCode = zipCodeTB.value
 
-    let result = await fetchZone(zipCode)
-    
-    let zone = `<div class="zoneDisplay">
-        <h2> Your Growing Zone </h2>
-        <span>Zone: ${result.zone}</span>
-        <span>Temp Range: ${result.temperature_range}</span>
-        <span>Lat: ${result.coordinates.lat}</span>
-        <span>Lon: ${result.coordinates.lon}</span>
-        </div>`    
+    const zipCodeRegex = /^\d{5}$/
 
-    resultsContainer.innerHTML = zone
-    zipCodeTB.value = ''
+    if (zipCodeRegex.test(zipCode)) {
+
+        let result = await fetchZone(zipCode)
+
+        let zone = `<div class="zoneDisplay">
+            <h2> Your Growing Zone </h2>
+            <span>Zone: ${result.zone}</span>
+            <span>Temp Range: ${result.temperature_range}</span>
+            <span>Lat: ${result.coordinates.lat}</span>
+            <span>Lon: ${result.coordinates.lon}</span>
+            </div>`
+
+        resultsContainer.innerHTML = zone
+        zipCodeTB.value = ''
+
+    } else {
+        resultsContainer.innerHTML = 'Please enter a valid zip code.'
+        zipCodeTB.value = ''
+    }
 })
 
 
